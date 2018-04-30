fdac8aa325b45122e57f044be4abc64b7af231bb
Mihail Tanev <mihailtanev84@gmail.com>
2018-04-30 20:55:27 +0200

    added domain entities and repositories

---
+++ StudentWebMarket.Web/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using StudentWebMarket.Data.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentWebMarket.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected IStudentWebMarketData db;
+
+        public BaseController(IStudentWebMarketData _db)
+        {
+            this.db = _db;
+        }
+        public BaseController()
+                : this(new StudentWebMarketData())
+        {
+        }
+    }
+}
