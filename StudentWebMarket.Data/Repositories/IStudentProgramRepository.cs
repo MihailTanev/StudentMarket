b26b01f9643ed58fd9f79b5366af2cd504e92cb8
Mihail Tanev <mihailtanev84@gmail.com>
2018-05-17 21:59:02 +0200

    added stud prog

---
+++ StudentWebMarket.Data/Repositories/IStudentProgramRepository.cs
@@ -0,0 +1,19 @@
+﻿using StudentWebMarket.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentWebMarket.Data.Repositories
+{
+    public interface IStudentProgramRepository
+    {
+        List<StudentProgram> FindAll();
+        StudentProgram GetById(object Id);
+        void Insert(StudentProgram Id);
+        void Update(StudentProgram Id);
+        void Delete(object Id);
+        void Save();
+    }
+}
