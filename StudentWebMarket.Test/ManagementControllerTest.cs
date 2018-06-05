ff5260e42888d97c72d2104d2650a035dd5fbe8e
Mihail Tanev <mihailtanev84@gmail.com>
2018-06-05 23:25:54 +0200

    added unit test

---
+++ StudentWebMarket.Test/ManagementControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentWebMarket.Web.Controllers;
+
+namespace StudentWebMarket.Test
+{
+    [TestClass]
+    public class ManagementControllerTest
+    {
+        [TestMethod]
+        public void Edit()
+        {
+            //Arrange
+            ManagementController cT = new ManagementController();
+
+            //Act
+            ViewResult result = cT.EditCategory(1) as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
