d3905761443e0e572b828197746a4b629826f159
Mihail Tanev <mihailtanev84@gmail.com>
2018-06-09 18:14:22 +0200

    modified admin panel

---
+++ StudentWebMarket.Web/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using StudentWebMarket.Data.EF;
+using StudentWebMarket.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentWebMarket.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private StudentWebMarketDbContext context = new StudentWebMarketDbContext();
+        public ActionResult ManageUsers()
+        {
+            var users = (from user in context.Users
+                                  select new
+                                  {
+                                      PhoneNumber=user.PhoneNumber,
+                                      UserId = user.Id,
+                                      Username = user.UserName,
+                                      FirstName = user.FirstName,
+                                      LastName=user.LastName,
+                                      Email = user.Email,
+                                      RoleNames = (from userRole in user.Roles
+                                                   join role in context.Roles on userRole.RoleId
+                                                   equals role.Id
+                                                   select role.Name).ToList()
+                                  }).ToList().Select(p => new UsersViewModel()
+
+                                  {
+                                      PhoneNumber=p.PhoneNumber,
+                                      FirstName = p.FirstName,
+                                      LastName=p.LastName,
+                                      UserId = p.UserId,
+                                      Username = p.Username,
+                                      Email = p.Email,
+                                      Role = string.Join(",", p.RoleNames)
+                                  });
+
+
+            return View(users);
+        }
+    }
+}
---
+++ StudentWebMarket.Web/ViewModels/UsersViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentWebMarket.Web.ViewModels
+{
+    public class UsersViewModel
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+
+
+    }
+}
